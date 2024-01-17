92698320e3cb4c16e46dcd38fd52c345128152b0
codeation <39458623+codeation@users.noreply.github.com>
2024-01-17 06:08:20 +0200

    Ignore configure duplicate

--- event.c
+++ event.c
@@ -49,6 +49,14 @@ static void write_configure_event() {
 }
 
 gboolean on_configure(GtkWindow *window, GdkEventConfigure *event, gpointer data G_GNUC_UNUSED) {
+    static gint x = 0, y = 0, width = 0, height = 0;
+    if (event->x == x && event->y == y && event->width == width && event->height == height) {
+        return FALSE;
+    }
+    x = event->x;
+    y = event->y;
+    width = event->width;
+    height = event->height;
     write_configure_event();
     return FALSE;
 }
